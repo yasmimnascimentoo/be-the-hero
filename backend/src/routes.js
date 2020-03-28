51eddd6e71dc40c6adf755f41070af91c8bed861
Yasmim Nascimento <Yasmimy.s82@gmail.com>
2020-03-28 11:45:58 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const ongController = require('./controllers/OngController');
+const incidentController = require('./controllers/incidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
+
+module.exports = routes;
